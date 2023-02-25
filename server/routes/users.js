ba2d56ce6e601b55ffb3a5201b3789a9d767e1fc
leonardocox <leonardocx0@gmail.com>
2023-02-25 17:05:50 -0500

    Added users route and added read and update

---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
