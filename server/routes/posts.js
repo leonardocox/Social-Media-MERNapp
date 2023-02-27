328adb2947aac4db074929122afe92b2b46bc997
leonardocox <leonardocx0@gmail.com>
2023-02-27 00:40:08 -0500

    daily commit

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,3 +1,10 @@
 import express from "express";
 import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
 import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/");
+
+/* UPDATE */
