14c0cc3377070b5fc0a9cae60a730ea6c98be8d3
leonardocox <leonardocx0@gmail.com>
2023-02-27 17:07:49 -0500

    posts route for when posting things to your page

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,6 +5,10 @@ import { verifyToken } from "../middleware/auth.js";
 const router = express.Router();
 
 /* READ */
-router.get("/");
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
 /* UPDATE */
+router.patch(":/id/like", verifyToken, likePost);
+
+export default router;
