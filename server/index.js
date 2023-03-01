622bec13314da4497b27ef924590e0c674474e25
leonardocox <leonardocx0@gmail.com>
2023-03-01 09:35:53 -0500

    saved

--- server/index.js
+++ server/index.js
@@ -65,7 +65,7 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    User.insertMany(users);
-    Post.insertMany(posts);
+    //User.insertMany(users);
+    //Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
