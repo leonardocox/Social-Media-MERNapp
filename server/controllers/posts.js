3ea0f723032434371579abd6c164a044d4291013
leonardocox <leonardocx0@gmail.com>
2023-03-01 09:52:38 -0500

    cleaned up errors

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import Post from "../models.Post.js";
+import Post from "../models/Post.js";
 import User from "../models/User.js";
 
 /* CREATE */
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,8 @@ import {
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth.js";
 
+const router = express.Router();
+
 /* READ */
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/friends", verifyToken, getUserFriends);
